94633e3abccbeb0d04ca40f8b8e0daf373ad2ee0
Hannah Joyce <93941057+HJoyce1@users.noreply.github.com>
2024-10-22 15:36:42 +0100

    small corrections

--- joy_model.py
+++ joy_model.py
@@ -10,28 +10,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-'''
-need to write a bit of code to extract pressure from data ie:
-    
-# read in solar wind paramters file
-df = pd.read_csv('/Users/hannah/OneDrive - Lancaster University/aurora/solar_wind_data.csv',delimiter=',')
-
-# need to convert columns to arrays to work with averages
-ram_pressure_array = df[['RAM_PRESSURE_PROTONS_NPA']].to_numpy() # pressure 
-
-ram_pressure_array[15000] # <-- need a way to get this location?
-
-don't need this anymore provided function carries over bs_loc and mp_loc
-'''
-
-pressure = 0.05
-
 # magnetopause location
 def magnetopause_location(pressure,plotting):
     # coefficients, joy et al 2002
     a_mag = -0.134 + (0.488*(pressure**(-0.25)))
-    b_mag = -0.581 - 0.225*((pressure**(-0.25)))
-    c_mag = -0.186 - 0.016*((pressure**(-0.25)))
+    b_mag = -0.581 - (0.225*(pressure**(-0.25)))
+    c_mag = -0.186 - (0.016*(pressure**(-0.25)))
     d_mag = -0.014 + (0.096*pressure)
     e_mag = -0.814 - (0.811*pressure)
     f_mag = -0.050 + (0.168*pressure)
@@ -83,7 +67,7 @@ def magnetopause_location(pressure,plotting):
     # gives location of magnetpopause in RJ
     mp_loc = x[min_minus_y]
     # print(mp_loc)
-    return mp_loc
+    return mp_loc#, x, y
 
 
 def multi_nose(pressure):
@@ -107,9 +91,9 @@ def bow_shock_location(pressure,plotting):
     a_mag = -1.107 + (1.591*(pressure**(-0.25)))
     b_mag = -0.566 - 0.812*((pressure**(-0.25)))
     c_mag = 0.048 - 0.059*((pressure**(-0.25)))
-    d_mag = 0.077 - (0.38*pressure)
+    d_mag = 0.077 - (0.038*pressure)
     e_mag = -0.874 - (0.299*pressure)
-    f_mag = -0.055 + (0.059*pressure)
+    f_mag = -0.055 + (0.124*pressure)
     
     # set values for x axis - in RJ
     x_plot = np.arange(-250,200,.01, dtype=float) # .01 for resolution purposes
@@ -163,8 +147,20 @@ def bow_shock_location(pressure,plotting):
     bs_loc = x[min_minus_y]
     #print(bs_loc)
     #breakpoint()
-    return bs_loc
+    return bs_loc#, x, y
 
-# to run function if ehecking from this file
-# magnetopause_location(pressure,'yes')
-# bow_shock_location(pressure,'yes')
+
+# # to run function if ehecking from this file
+# pressure = 0.6
+# mp_loc, x1, y1 = magnetopause_location(pressure,'yes')
+# bs_loc, x2, y2 = bow_shock_location(pressure,'yes')
+
+# print(mp_loc)
+# print(bs_loc)
+
+# fig = plt.figure()
+# ax1 = fig.add_subplot(1,1,1)
+# ax1.plot(x1, y1)
+# ax1.plot(x2, y2)
+# ax1.tick_params(which='both',direction='in',bottom=True, top=True, left=True, right=True)
+# ax1.set_xlim(0,60)
